b4aa11506bc02ece38d37e2f226013b17cd1a20d
code-gram <pankajsharma.yash@gmail.com>
2020-11-25 16:45:02 +0530

    BindingResult introduced

--- src/main/java/in/codegram/ppmtoolapi/web/ProjectController.java
+++ src/main/java/in/codegram/ppmtoolapi/web/ProjectController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,10 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project) {
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+		}
 		Project newProject = projectService.saveOrUpdate(project);
 		return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
 	}
